6258234cd949af2bb224681ce828b4df0524fceb
CaraRota <canalsdigital@gmail.com>
2023-12-07 18:33:05 -0300

    Fix fonts and sendBtn

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,16 +10,25 @@ import instagram from '../../assets/images/ig-icon-white.png'
 import facebook from '../../assets/images/fb-icon-white.png'
 
 const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
     return (
         <div className='footerContainer'>
             <div className='social'>
                 <div className='socialContainer'>
-                    <img className='instagram' src={instagram}></img>
-                    <img className='facebook' src={facebook}></img>
+                    <a href="https://www.instagram.com/" target='_blank'><img className='instagram' src={instagram}></img></a>
+                    <a href="https://www.facebook.com/" target='_blank'><img className='facebook' src={facebook}></img></a>
                 </div>
                 <div className='text'>Seguinos en nuestras redes</div>
             </div>
-            <img className='logo' src={logo}></img>
+            <a className='goTop' onClick={scrollToTop}>
+                <img className='logo' src={logo}></img>
+            </a>
             <div className='newsletter'>
                 <div className='text'>¡Suscribite a nuestro newsletter!</div>
                 <input className='email' type="text" placeholder="correo@mail.com"></input>
