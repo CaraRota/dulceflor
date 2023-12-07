6d231271453ccfc9b9008f706f6c344145b50fad
CaraRota <canalsdigital@gmail.com>
2023-12-06 22:22:47 -0300

    Add fonts/SASS/imagery

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../../css/Footer.css'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer-font'>Footer</div>
     )
 }
 
