df2f428539f8e9ed5f76fac4a53a10f4a414a9fa
kari <karifernandezmorales@gmail.com>
2023-12-08 17:16:34 -0300

    first navbar commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './navbar.scss';
+
+
+
+//Images
+import logoNavbar from '../../assets/images/logoNavbar.png'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbarContainer'>
+
+            <div className='navbarLogo'>
+                <img className='logoNavbar' src={logoNavbar}></img>
+            </div>
+            
+            <div className='divPagesNavbar'>
+                <p className='pagesNavbar'>Inicio</p>
+                <p className='pagesNavbar'>Productos</p>
+                <p className='pagesNavbar'>Propuesta</p>
+                <p className='pagesNavbar'>Contacto</p>
+            </div>
+                
+        </div>
     )
 }
 
