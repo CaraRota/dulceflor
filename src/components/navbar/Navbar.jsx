2dc4d302428492584be276ac4c54e75ca2a03ccf
CaraRota <canalsdigital@gmail.com>
2023-12-08 22:06:26 -0300

    Fix css import and add RRD routing with Links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
-import './navbar.scss';
+import '../../css/navbar.css';
 
-
+//RRD
+import { Link } from 'react-router-dom'
 
 //Images
 import logo from '../../assets/images/logo-transparent.png'
 
 const Navbar = () => {
     return (
-        <div className='navbarContainer'>
-
-            <div className='navbarLogo'>
-                <img className='logoNavbar' src={logo}></img>
-            </div>
-            
-            <div className='divPagesNavbar'>
-            <a><p className='pagesNavbar'>Inicio</p></a>
-              
-                <a href='#productos-section'>
-                    {/* "productos-section" o el id que corresponda */}
+        <>
+            <div className='navbarContainer'>
+                <div className='navbarLogo'>
+                    <Link to='/'>
+                        <img className='logoNavbar' src={logo}></img>
+                    </Link>
+                </div>
+                <nav className='divPagesNavbar'>
+                    <Link to='/'>
+                        <p className='pagesNavbar'>Inicio</p>
+                    </Link>
+                    <Link to='/products'>
                         <p className='pagesNavbar'>Productos</p>
-                </a>
-                
-                <a><p className='pagesNavbar'>Propuesta</p></a>
-                <a><p className='pagesNavbar'>Contacto</p></a>
+                    </Link>
+                    <Link to='/propuesta'>
+                        <p className='pagesNavbar'>Propuesta</p>
+                    </Link>
+                    <Link to='/contact'>
+                        <p className='pagesNavbar'>Contacto</p>
+                    </Link>
+                </nav>
             </div>
-                
-        </div>
+        </>
     )
 }
 
