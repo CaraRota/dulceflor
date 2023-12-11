7066b294acf8382bbd7067cfa0a0eeaf5952ebf7
CaraRota <canalsdigital@gmail.com>
2023-12-11 13:44:18 -0300

    Add all  sections

--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -11,14 +11,17 @@ import petalosAdelante from '../../../assets/images/flor-adelante.png'
 import gajoNaranja from '../../../assets/images/gajo-naranja.png'
 import rodajaNaranja from '../../../assets/images/rodaja-naranja.png'
 
+//Images for sections
+import frascoPera from '../../../assets/images/frasco-pera.png'
+import frascoFrutilla from '../../../assets/images/frasco-frutilla.png'
+import frascoMandarina from '../../../assets/images/frasco-mandarina.png'
+import frascoFrambuesa from '../../../assets/images/frasco-frambuesa.png'
+
 //RRD
 import { Link } from 'react-router-dom'
 
 //Components
 import Section1 from './Section1'
-import Section2 from './Section2'
-import Section3 from './Section3'
-import Section4 from './Section4'
 
 const Home = () => {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -46,9 +49,10 @@ const Home = () => {
                         <h1>Conocé otra manera de regalar flores</h1>
                     </div>
                     <div className='subtitle'>
-                        <p>Elegí entre nuestra variedad de únicos y exclusivos
-                            <p>sabores de <span className='bold'>mermeladas</span> hechas con <span className='bold'>pétalos de flores.</span></p>
+                        <p>
+                            Elegí entre nuestra variedad de únicos y exclusivos
                         </p>
+                        <p>sabores de <span className='bold'>mermeladas</span> hechas con <span className='bold'>pétalos de flores.</span></p>
                     </div>
                     <div className='contactBtn'>
                         <Link to={'/contact'}><SendBtn className='sendBtn' text='Contactanos' /></Link>
@@ -62,10 +66,59 @@ const Home = () => {
                     <img className='frascoPomelo' src={frascoPomelo} alt='frasco de pomelo' />
                 </div>
             </div>
-            <Section1 />
-            <Section2 />
-            <Section3 />
-            <Section4 />
+            <Section1
+                title1='Pomelo'
+                title2='con rosas'
+                text='Equilibrio entre la intensidad cítrica del pomelo con la suavidad floral de los pétalos de rosas.'
+                text2='Experimentá una combinación excepcional al untar esta mermelada en tostadas de pan integral con queso brie.'
+                text3='El contraste de sabores crea una experiencia única.'
+                image={frascoPomelo}
+                leftSide={true}
+                alt='frasco de pomelo'
+
+            />
+            <Section1
+                title1='Pera'
+                title2='con jazmín'
+                text='Ideal fusión entre la jugosa dulzura de la pera con la sutil fragancia del Jazmín.'
+                text2={null}
+                text3='Usá esta mermelada para acompañar un rico té de jazmín y alguna preparación con masa madre (ya que su acidez 
+                    equilibra los sabores).                    '
+                image={frascoPera}
+                leftSide={false}
+                alt='frasco de pera'
+            />
+            <Section1
+                title1='Frutilla'
+                title2='con rosas'
+                text='Perfecta armonía entre la frescura de las frutillas y la delicadeza floral de los pétalos de rosas.'
+                text2={null}
+                text3='Saboreá una tarta de chocolate amargo, rellena con mermelada de fresas y rosas, cubierta con virutas de chocolate y
+                decorada con pétalos.'
+                image={frascoFrutilla}
+                leftSide={true}
+                alt='frasco de frutilla'
+            />
+            <Section1
+                title1='Mandarina'
+                title2='con azafrán'
+                text='Adecuado balance entre la vibrante frescura de la mandarina y la exquisita calidez del azafrán.'
+                text2={null}
+                text3='Dale un toque gourmet a tu desayuno combinando esta mermelada con pan de centeno y queso en feta.'
+                image={frascoMandarina}
+                leftSide={false}
+                alt='frasco de mandarina'
+            />
+            <Section1
+                title1='Frambuesa'
+                title2='y champagne con rosas'
+                text='Justa combinación entre la acidez de la frambuesa, las burbujas refinadas del champagne y la presencia de los pétalos de rosas.'
+                text2={null}
+                text3='Dale un toque gourmet a tu desayuno combinando esta mermelada con pan de centeno y queso en feta.'
+                image={frascoFrambuesa}
+                leftSide={true}
+                alt='frasco de frambuesa'
+            />
         </>
     )
 }
--- src/components/pages/homepage/Section1.jsx
+++ src/components/pages/homepage/Section1.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
 
-const Section1 = () => {
+//CSS
+import '../../../css/Section1.css'
+
+const Section1 = ({ title1, title2, text, text2, text3, image, leftSide, alt }) => {
+
     return (
-        <div>Section1</div>
+
+        leftSide ? (
+            <div className='sectionContainer'>
+                <div className='leftSide'>
+                    <div className='title'>
+                        {title2.length < 20 ?
+                            (
+                                <>
+                                    <span id={title1} className='specialFontTitle'>{title1}</span>
+                                    <span className='normalFontTitle'> {title2}</span>
+                                </>
+                            )
+                            : (
+                                <>
+                                    <div id={title1} className='longSpecialFontTitle'>{title1}</div>
+                                    <div className='longFontTitle'> {title2}</div>
+                                </>
+                            )
+                        }
+                    </div>
+                    <div className='subtitle'>
+                        <p className='text'>
+                            {text}
+                        </p>
+                    </div>
+                    <div className={`rectangle ${title1}`}>
+                        <div className='text'>{text2}
+                            <p>{text3}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='rightSide'>
+                    <div id={title1} className='wrapper'>
+                        <img className='frascoLeft' src={image} alt={alt} />
+                    </div>
+                </div>
+            </div>
+        )
+            : (
+                <div className='sectionContainer'>
+                    <div className='rightSide'>
+                        <div id={title1} className='wrapper'>
+                            <img className='frasco' src={image} alt={alt} />
+                        </div>
+                    </div>
+                    <div className='leftSideLeft'>
+                        <div className='title'>
+                            {title2.length < 20 ?
+                                (
+                                    <>
+                                        <span id={title1} className='specialFontTitle'>{title1}</span>
+                                        <span className='normalFontTitle'> {title2}</span>
+                                    </>
+                                )
+                                : (
+                                    <>
+                                        <div id={title1} className='longSpecialFontTitle'>{title1}</div>
+                                        <div className='longFontTitle'> {title2}</div>
+                                    </>
+                                )
+                            }
+                        </div>
+                        <div className='subtitle'>
+                            <p className='text'>
+                                {text}
+                            </p>
+                        </div>
+                        <div className={`rectangle ${title1}`}>
+                            <div className='text'>{text2}
+                                <p>{text3}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
     )
 }
 
