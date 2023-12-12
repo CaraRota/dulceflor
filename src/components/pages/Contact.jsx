05574634d933750ac2106a2169e89a3210cd4a8d
CaraRota <canalsdigital@gmail.com>
2023-12-12 16:38:04 -0300

    Add email sender with emailJS

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //Components
 import Frasco from '../ui/frasco/Frasco'
 import SendBtn from '../ui/buttons/SendBtn'
 
+//Config
+import { configEmailJS } from '../../config/config.js'
+
+//EmailJS
+import emailjs from '@emailjs/browser';
+
 //CSS
 import '../../css/Contact.css'
 
 //RHF
-import { useForm } from 'react-hook-form';
+import { set, useForm } from 'react-hook-form';
 
 const Contact = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+
+    const onSubmit = async (data, e) => {
+        setIsLoading(true);
+        e.preventDefault();
+
+        const { Nombre, Celular, Email, Mensaje } = data;
+
+        const templateParams = {
+            to_name: 'Dulce Flor',
+            from_name: Nombre.trim(),
+            phone: Celular.trim(),
+            email: Email.trim(),
+            message: Mensaje.trim(),
+            reply_to: Email.trim()
+        };
+
+        emailjs.send(configEmailJS.serviceID, configEmailJS.templateID, templateParams, configEmailJS.userID)
+            .then((result) => {
+                console.log(result.text);
+                setIsLoading(false);
+            }, (error) => {
+                console.log(error.text);
+                setIsLoading(false);
+            });
+
+        e.target.reset();
+
+    };
 
     return (
         <div className='contactContainer'>
@@ -29,20 +62,85 @@ const Contact = () => {
                     <div className='labelContainer'>
                         <label>Ingresá tu nombre</label>
                     </div>
-                    <input type="text" placeholder="Nombre" {...register("Nombre", { required: true, maxLength: 80 })} />
+                    {errors.Nombre?.type === "required" &&
+                        <span className='error'>El nombre es requerido</span>
+                    }
+                    {errors.Nombre?.type === "pattern" &&
+                        <span className='error'>El nombre debe contener caracteres válidos</span>
+                    }
+                    {errors.Nombre?.type === "maxLength" &&
+                        <span className='error'>El nombre no puede tener más de 80 caracteres</span>
+                    }
+                    <input type="text" placeholder="Nombre" {...register("Nombre",
+                        {
+                            required: true,
+                            maxLength: 80,
+                            pattern: /^[A-Za-z\s]+$/i
+                        })} />
                     <div className='labelContainer'>
                         <label>Ingresá tu número celular</label>
                     </div>
-                    <input type="tel" placeholder="Número celular" {...register("Celular", { required: true })} />
+                    {errors.Celular?.type === "required" &&
+                        <span className='error'>El número celular es requerido</span>
+                    }
+                    {errors.Celular?.type === "pattern" &&
+                        <span className='error'>El número celular sólo puede contener números</span>
+                    }
+                    {(
+                        errors.Celular?.type === "minLength" ||
+                        errors.Celular?.type === "maxLength"
+                    ) && (
+                            <span className='error'>El número celular debe tener entre 8 y 12 caracteres</span>
+                        )}
+                    <input type="tel" placeholder="Número celular" {...register("Celular",
+                        {
+                            required: true,
+                            minLength: 8,
+                            maxLength: 12,
+                            pattern: /^[0-9]+$/i
+                        })} />
                     <div className='labelContainer'>
                         <label>Ingresá tu correo electrónico</label>
                     </div>
-                    <input type="text" placeholder="Correo electrónico" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
+                    {errors.Email?.type === "required" &&
+                        <span className='error'>El correo electrónico es requerido</span>
+                    }
+                    {errors.Email?.type === "pattern" &&
+                        <span className='error'>El correo electrónico debe ser válido</span>
+                    }
+                    {errors.Email?.type === "maxLength" &&
+                        <span className='error'>El correo electrónico no puede tener más de 80 caracteres</span>
+                    }
+                    <input type="text" placeholder="Correo electrónico" {...register("Email",
+                        {
+                            required: true,
+                            pattern: /^\S+@\S+$/i,
+                            maxLength: 80
+                        })} />
                     <div className='labelContainer'>
                         <label>Ingresá tu mensaje</label>
                     </div>
-                    <textarea placeholder='Mensaje' {...register("Mensaje", { required: true })} />
-                    <SendBtn type="submit" text={'Enviar'} />
+                    {errors.Mensaje?.type === "required" &&
+                        <span className='error'>El mensaje es requerido</span>
+                    }
+                    {(errors.Mensaje?.type === "maxLength" ||
+                        errors.Mensaje?.type === "minLength"
+                    ) && (
+                            <span className='error'>El mensaje debe contener entre 10 y 500 caracteres</span>
+                        )
+                    }
+                    <textarea placeholder='Mensaje' {...register("Mensaje", {
+                        required: true,
+                        maxLength: 500,
+                        minLength: 10,
+                    })} />
+                    {
+                        !isLoading ? (
+                            <SendBtn type="submit" text={'Enviar'} />
+                        ) : (
+                            <SendBtn type="submit" text={'Enviando...'} />
+                        )
+                    }
                 </form>
             </div>
             <div className='rightSide'>
