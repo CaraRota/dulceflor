a4ed31bbf24aa0ab65c23b147cbc70db520d1406
kari <karifernandezmorales@gmail.com>
2023-12-13 09:34:06 -0300

    adding material ui navbar

--- src/components/pages/Propuesta.jsx
+++ src/components/pages/Propuesta.jsx
@@ -3,21 +3,32 @@ import React from 'react'
 
 import './Propuesta.scss'
 
+//Images
+import propuesta from '../../assets/images/foto-propuesta.png'
+
 
 const Propuesta = () => {
     return (
         <div className='propuesta-title'>
-            <h1>
-                <span className="black-text">DE MENDOZA, CUNA DE </span>
-                <span className="purple-text">flores de calidad</span>
-            </h1>
-            <div className='propuesta-text'>
-                <p>El clima seco y el agua pura de montaña en Mendoza, junto con sistemas de riego controlados, son ideales para el <span className="">cultivo orgánico</span>. Esta combinación única, cuidada por manos expertas, produce flores excepcionales que no solo adornan, sino que también dan vida a nuestras exquisitas mermeladas.
+            <div className='content-container'>
+                <div className='text-container'>
+                    <div className='title-container'>
+                        <h1>
+                        <span className="black-text">DE MENDOZA, CUNA DE </span>
+                        <span className="purple-text">flores de calidad</span>
+                        </h1>
+                    </div>
+                    <div className='propuesta-text'>
+                        <p>El clima seco y el agua pura de montaña en Mendoza, junto con sistemas de riego controlados, son ideales para el <span>cultivo orgánico</span>. Esta combinación única, cuidada por manos expertas, produce flores excepcionales que no solo adornan, sino que también dan vida a nuestras exquisitas mermeladas.</p>
 
-                Mendoza, reconocida por sus vinos de excelencia, es también un santuario floral. Cada pétalo narra una historia de dedicación y compromiso. Con estas flores, elaboramos artesanalmente mermeladas que capturan la <span className="">esencia</span> de <span className="">nuestra tierra</span>, ofreciendo una experiencia culinaria única y auténtica.
-                </p>
+                        <p>Mendoza, reconocida por sus vinos de excelencia, es también un santuario floral. Cada pétalo narra una historia de dedicación y compromiso. Con estas flores, elaboramos artesanalmente mermeladas que capturan la <span>esencia</span> de <span className="">nuestra tierra</span>, ofreciendo una experiencia culinaria única y auténtica.</p>
+                    </div>
+                </div>
+
+                <div className='image-container'>
+                    <img src={propuesta} alt="Propuesta"/>
+                </div>
             </div>
-            
         </div>
     )
 }
