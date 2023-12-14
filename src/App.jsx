e7a7413579d31bf87b81fa377220d9c25162b828
CaraRota <canalsdigital@gmail.com>
2023-12-14 10:38:04 -0300

    Fix to css and css imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //Main pages of the website
 import Home from './components/pages/homepage/Home.jsx'
-import Products from './components/pages/Products.jsx'
 import Propuesta from './components/pages/Propuesta.jsx'
 import Contact from './components/pages/Contact.jsx'
 import ErrorPage from './components/notfoundpage/ErrorPage.jsx'
@@ -23,7 +22,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/propuesta" element={<Propuesta />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/products' element={<Products />} />
+        <Route path='/propuesta' element={<Propuesta />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
       <Footer />
